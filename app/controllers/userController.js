773ed54a123bf0ca984320459950a629721e043a
Taha <t.hassouna@hotmail.fr>
2022-11-14 09:51:41 +0100

    Ajout commentaires (User)

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,8 +3,10 @@ const User = require("../models/user");
 
 User.findAll((error, users) => {
     if (error) {
+        //Le controller choisis l'action à faire en cas d'erreur
        console.log( "attention", error);
     }else {
+        //si tout est OK le controller log l'instance de User qui a parsé coté Models
         console.log(users);
     }
 });
--- app/models/user.js
+++ app/models/user.js
@@ -7,6 +7,7 @@ class User extends CoreModel {
   #firstname;
   #lastname;
 
+  //le constructor passe par les setters
   constructor(obj) {
     super(obj);
     this.email = obj.email;
@@ -15,7 +16,7 @@ class User extends CoreModel {
     this.lastname = obj.lastname;
   }
 
-  //GETTERS
+  //GETTERS : : Je récupére les valeurs des propriétés privées
   get email() {
     return this.#email;
   }
@@ -32,7 +33,7 @@ class User extends CoreModel {
     this.#lastname;
   }
 
-  //SETTERS
+  //SETTERS: Je modifie les valeurs des propriétés privées
   set email(value) {
     this.#email = value;
   }
@@ -49,14 +50,26 @@ class User extends CoreModel {
     this.#lastname = value;
   }
 
+
+  //Méthode Active Record
+
+  // callback est la fonction du controller qui attends d'être excécutée
  static findAll(callback) {
+    //une fois la requette sql est achevée, alors quelque chose sera exécutée (elle attends une erreur ou un resultat)
+    //Je ne veux pas renvoyer les objets en dur donc je les parses
     client.query(`SELECT * FROM "user"`, (error, result) => {
+
+        //en cas d'erreur, j'appelle le traitement du controller en lui passant l'erreur et rien en resultat
         if(error) callback(error, null);
         else{
+            // sinon j'effectues le parse: je crée un array dans leqeul je push les instances
             const users = [];
+            // pour pouvoir faire un instance sur chaque user je boucle sur le resultat en base de données
             for (const user of result.rows) {
+                //Puis, je push dans le tableau une instance représentant l'user actuellement parcouru
                 users.push(new User(user));
             }
+            //en cas de succès j'effectues le traitement du controller en lui passant le tableau mais pas d'erreur
             callback(null, users);
         }
     })
