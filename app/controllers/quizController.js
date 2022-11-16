9f0e948d680a463ea0d6e6960dc0cc7ebe6ee17d
Taha <t.hassouna@hotmail.fr>
2022-11-16 16:10:08 +0100

    Test plusieurs jointures findAll sur Quiz

--- app/controllers/quizController.js
+++ app/controllers/quizController.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 const {Quiz} = require("../models");
 
 Quiz.findAll({
-    include: 'author',
+    include: [
+        'author',
+        'questions'
+    ]
 }).then((quizzes) =>{
     console.log(quizzes);
 }).catch((error) =>{
