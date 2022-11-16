15873e58c077f13a6655f7c19988a086d0f00cab
Taha <t.hassouna@hotmail.fr>
2022-11-16 12:18:24 +0100

    Test méthode create() sur User

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,16 +1,28 @@
 require("dotenv").config();
 const User = require("../models/user");
 
-
 /*
 User.findAll().then(users => {
   console.log(users);
  }).catch(error => {
   console.log(error);
- }); */
+ }); 
 
  User.findByPk(1).then(users => {
   console.log(users);
  }).catch(error => {
   console.log(error);
- });
+ });*/
+
+User.create({
+  email: "taha@free.fr",
+  password: "12345",
+  firstname: "taha",
+  lastname: "hass",
+
+}).then((user) => {
+  console.log(user);
+
+}).catch((error) => {
+    console.log(error);
+});
